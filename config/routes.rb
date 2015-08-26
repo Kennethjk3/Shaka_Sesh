d5613b986ecdeaa8a158798f410ef920032e9bcb
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-08-26 09:47:30 -0400

    roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
+
+  devise_scope :user do
+  authenticated :user do
+    root 'users#show', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'visitors#index', as: :unauthenticated_root
+  end
+end
   resources :users
   resources :dashes
 end
