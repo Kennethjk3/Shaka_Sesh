66e4a97f7aa4ecb5a5fc3011fd00b46357eedb6b
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-08-29 04:24:14 -0400

    more aws

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 has_attached_file :avatar,
-  styles: { medium: "300x300>", thumb: "100x100>" },
+  styles: {  thumb: "100x100>", square: '200x200#', medium: "300x300>" },
   default_url: "/images/:style/missing.png"
   :storage => :s3,
-  :bucket => 'hohohohoho',
   :s3_credentials => "#{Rails.root}/config/aws.yml",
   :path => "resources/:id/:style/:basename.:extension"
-validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+validates_attachment_content_type :avatar,
+  content_type: /\Aimage\/.*\Z/
 
 end
