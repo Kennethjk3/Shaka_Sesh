747f77d51101b5b50097f4e3b66920b19ae17623
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-08-29 03:51:55 -0400

    user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
+has_attached_file :avatar,
+  styles: { medium: "300x300>", thumb: "100x100>" },
   default_url: "/images/:style/missing.png"
   :storage => :s3,
   :bucket => 'hohohohoho',
   :s3_credentials => "#{Rails.root}/config/aws.yml",
-  :path => "resources/:id/:style/:basename.:extension
+  :path => "resources/:id/:style/:basename.:extension"
 validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 end
