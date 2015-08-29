d21b87905320de9433712580beeeee7f0c0149d3
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-08-28 23:37:14 -0400

    pic support?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,11 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_filter :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+    def after_sign_in_path_for(resource)
+      current_user
+    end
 
+    protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :avatar) }
         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
